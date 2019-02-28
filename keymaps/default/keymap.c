dd091a1aa69c49cbd5611e4edb52015ddd0966c9
Luca Masera <luca.masera@aau.at>
2019-02-28 07:49:39 +0100

    With media keys

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -21,7 +21,7 @@ enum CustomLayers {
         _SYMB,
         _FUNC,
         _NUMS,
-        _NAVI
+        _NVMD
 };
 
 enum CustomKeycodes {
@@ -37,12 +37,12 @@ enum CustomKeycodes {
 #define OSM_LALT  OSM(MOD_LALT)
 #define TO_FUNC   OSL(_FUNC)
 #define TO_SYMB   OSL(_SYMB)
-#define NAVI_TAB  LT(_NAVI, KC_TAB)
+#define NVMD_TAB  LT(_NVMD, KC_TAB)
 #define NUMS_ESC  LT(_NUMS, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_BASE] = {
-                { NAVI_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,       KC_MINS, /**/ KC_EQL,        KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,    NUMS_ESC}, \
+                { NVMD_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,       KC_MINS, /**/ KC_EQL,        KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,    NUMS_ESC}, \
                 { OSM_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,      KC_G,       KC_LBRC, /**/ KC_RBRC,       KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN, OSM_LALT}, \
                 {   KC_INS,    KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,      KC_GRAVE, /**/ KC_QUOTE,      KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS}, \
                 {    KC_NO, KC_HOME, KC_PGUP,  KC_END, TO_FUNC,    KC_SPC,      OSM_LSFT, /**/ KC_BSPC,       KC_ENT,     TO_SYMB, KC_LEFT, KC_UP,   KC_RGHT, KC_RALT}, \
@@ -56,8 +56,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 {    KC_NO,   KC_NO, KC_TRNS,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO}
         },
         [_FUNC] = {
-                {    KC_NO,   KC_F9,  KC_F10,  KC_F12,   KC_NO,     KC_NO,        KC_APP, /**/ KC_LGUI,       KC_NO,      KC_F4,   KC_F5,   KC_F6,   KC_NO,   KC_ESC}, \
-                {  KC_TRNS,   KC_F7,   KC_F8,   KC_F8,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_F1,   KC_F2,   KC_F3,   KC_NO,   KC_TRNS}, \
+                {    KC_NO,  KC_F10,  KC_F11,  KC_F12,   KC_NO,     KC_NO,        KC_APP, /**/ KC_LGUI,       KC_NO,      KC_F4,   KC_F5,   KC_F6,   KC_NO,   KC_ESC}, \
+                {  KC_TRNS,   KC_F7,   KC_F8,   KC_F9,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_F1,   KC_F2,   KC_F3,   KC_NO,   KC_TRNS}, \
                 {    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO}, \
                 {  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,       KC_TRNS, /**/ KC_DEL,        KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}, \
                 {    KC_NO,   KC_NO, KC_TRNS,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_TRNS, KC_NO,   KC_NO}
@@ -69,12 +69,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 {    KC_NO,   KC_NO,    KC_0,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO}, \
                 {    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO}
         },
-        [_NAVI] = {
+        [_NVMD] = {
                 {  KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_WH_U, KC_NO,   KC_NO,   RESET}, \
                 {    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_WH_L, KC_WH_D, KC_WH_R, KC_NO,   KC_NO}, \
-                {    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO}, \
-                {    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO}, \
-                {    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO}
+                {    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_MUTE, KC_NO,   KC_NO}, \
+                {    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_MNPV, KC_VOLU, KC_MNXT, KC_MPLY}, \
+                {    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_VOLD, KC_NO,   KC_NO}
         }
 };
 // {           KC_ESC,    KC_Q,    KC_W,    KC_E,      KC_R,    KC_T}, ,              KC_T,/**/ KC_Y,              KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,     KC_INS},
@@ -212,7 +212,7 @@ uint32_t layer_state_set_user(uint32_t state) {
         case _NUMS:
                 rgblight_setrgb_at(160, 0, 0, 2);
                 break;
-        case _NAVI:
+        case _NVMD:
                 rgblight_setrgb_at(160, 0, 0, 3);
                 break;
         // case 2:
