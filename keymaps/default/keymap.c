99dbc9bc7f56b30bd4599d49780e8d3f51be4d39
Luca Masera <luca.masera@aau.at>
2019-02-28 09:29:34 +0100

    Small change in layout

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -18,10 +18,10 @@
 
 enum CustomLayers {
         _BASE = 0,
-        _SYMB,
-        _FUNC,
-        _NUMS,
-        _NVMD
+        _SYMB,  // symbols
+        _FUNC,  // func keys
+        _NUMS,  // num keypad on left side
+        _NVMD   // media and navigation keys
 };
 
 enum CustomKeycodes {
@@ -77,50 +77,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 {    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,         KC_NO, /**/ KC_NO,         KC_NO,      KC_NO,   KC_NO,   KC_VOLD, KC_NO,   KC_NO}
         }
 };
-// {           KC_ESC,    KC_Q,    KC_W,    KC_E,      KC_R,    KC_T}, ,              KC_T,/**/ KC_Y,              KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,     KC_INS},
-// {             KC_A,    KC_A,    KC_S,    KC_D,      KC_F,    KC_G},               KC_G,/**/ KC_H,              KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN,  KC_LGUI},
-// {           KC_APP,    KC_Z,    KC_X,    KC_C,      KC_V,    KC_B},       TD(OSM_CTL),/**/ OSM_ALT,           KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH,  KC_RALT },
-// {            KC_NO, KC_HOME, KC_PGUP,  KC_END,    KC_SPC,  KC_SPC},            OSM_SFT,/**/ KC_BSPC,           KC_ENT,  KC_ENT,    KC_LEFT, KC_UP,   KC_RIGHT, RESET},
-// {            KC_NO,   KC_NO, KC_PGDN,   KC_NO,     KC_NO,   KC_NO}               KC_NO,/**/ KC_NO,             KC_NO,   KC_NO,     KC_NO,   KC_DOWN, KC_NO,    KC_NO}
-
-
-//
-// {LT(_NAVI, KC_TAB),    KC_A,    KC_S,    KC_D,      KC_F,    KC_G, TD(TD_L_BRACKETS),        TD(TD_R_BRACKETS), KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN,  KC_LGUI},
-// {           KC_ESC,    KC_Q,    KC_W,    KC_E,      KC_R,    KC_T,     TD(TD_MIN_AC),        TD(TD_EQL_GR),     KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,     KC_INS},
-// {            KC_NO, KC_HOME, KC_PGUP,  KC_END,OSL(_FUNC),  KC_SPC,           OSM_SFT,        KC_BSPC,           KC_ENT,  OSL(_SYMB),KC_LEFT, KC_UP,   KC_RIGHT, MO(_MEDIA)},
-// [_FUNC] = {
-//         {          KC_TRNS,   KC_NO,  KC_F10,  KC_F11,    KC_F12,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_F4,     KC_F5,   KC_F6,   KC_NO,    KC_TRNS},
-//         {          KC_TRNS,   KC_NO,   KC_F7,   KC_F8,     KC_F9,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_F1,     KC_F2,   KC_F3,   KC_NO,    KC_TRNS},
-//         {          KC_TRNS,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,           KC_TRNS,      KC_TRNS,           KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_TRNS},
-//         {          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,   KC_NO,           KC_TRNS,      KC_DEL,            KC_NO,   KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS},
-//         {            KC_NO,   KC_NO, KC_TRNS,   KC_NO,     KC_NO,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_NO,     KC_NO,   KC_TRNS, KC_NO,    KC_NO}
-// },
-// [_SYMB] = {
-//         {          KC_TRNS,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_TRNS},
-//         {          KC_TRNS,    KC_1,    KC_2,    KC_3,      KC_4,    KC_5,             KC_NO,      KC_NO,             KC_6,    KC_7,      KC_8,    KC_9,    KC_0,     KC_TRNS},
-//         {          KC_TRNS,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,           KC_TRNS,      KC_TRNS,           KC_NO,   KC_GRV,    KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS},
-//         {          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,   KC_NO,           KC_TRNS,      KC_TRNS,           KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS},
-//         {            KC_NO,   KC_NO, KC_TRNS,   KC_NO,     KC_NO,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_NO,     KC_NO,   KC_TRNS, KC_NO,    KC_NO}
-// },
-// [_NAVI] = {
-//         {            KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_NO,     KC_WH_U, KC_NO,   KC_NO,    KC_NO},
-//         {            KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_WH_L,   KC_WH_D, KC_WH_R, KC_NO,    KC_NO},
-//         {            KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,           KC_TRNS,      KC_TRNS,           KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_NO},
-//         {          KC_TRNS,CK_DSK_L, KC_TRNS,CK_DSK_R,   KC_TRNS,   KC_NO,           KC_TRNS,      KC_TRNS,           KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,  KC_NO},
-//         {            KC_NO,   KC_NO, KC_TRNS,   KC_NO,     KC_NO,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_NO,     KC_NO,   KC_TRNS, KC_NO,    KC_NO}
-// },
-// [_MEDIA] = {
-//         {          KC_SLEP,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,    RESET},
-//         {            KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_NO},
-//         {            KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_NO},
-//         {          KC_TRNS, KC_MPRV, KC_VOLU, KC_MNXT,   KC_MPLY, KC_MUTE,             KC_NO,      KC_NO,             KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_TRNS},
-//         {            KC_NO,   KC_NO, KC_VOLD,   KC_NO,     KC_NO,   KC_NO,             KC_NO,      KC_NO,             KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_NO}
-// }
-//
 
 void matrix_init_user(void) {
         rgblight_enable_noeeprom();
-        // rgblight_sethsv(0,0,125);
 }
 
 
@@ -132,14 +91,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         switch (keycode) {
         case TWO_AND:
                 if (record->event.pressed) {
-                        // when keycode QMKBEST is pressed
                         SEND_STRING(" && ");
                 }
                 break;
 
         case TWO_OR:
                 if (record->event.pressed) {
-                        // when keycode QMKBEST is pressed
                         SEND_STRING(" || ");
                 }
                 break;
@@ -152,7 +109,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case DB_QUOTE:
                 if (record->event.pressed) {
-                        // when keycode QMKBEST is pressed
                         SEND_STRING("\" ");
                 }
                 break;
