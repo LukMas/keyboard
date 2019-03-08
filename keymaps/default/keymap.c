b1934c7796ebc0d60a4a542dcbe884e16cd32bc4
Luca Masera <luca.masera@aau.at>
2019-03-08 13:16:58 +0100

    Corrected position of F keys

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -65,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     KC_TRNS,                                         /**/                                         KC_TRNS
         ),
         [_FUNC] = LAYOUT(
-                   KC_TAB,  KC_F10,  KC_F11,  KC_F12,   KC_NO,     KC_NO,    KC_APP, /**/ KC_LGUI,  KC_NO,      KC_F4,   KC_F5,   KC_F6,   KC_NO,   KC_ESC, \
-                  KC_TRNS,   KC_F7,   KC_F8,   KC_F9,   KC_NO,     KC_NO,     KC_NO, /**/ KC_NO,    KC_NO,      KC_F1,   KC_F2,   KC_F3,   KC_NO,   KC_TRNS, \
+                   KC_TAB,   KC_NO,  KC_F10,   KC_F11, KC_F12,     KC_NO,    KC_APP, /**/ KC_LGUI,  KC_NO,      KC_F4,   KC_F5,   KC_F6,   KC_NO,   KC_ESC, \
+                  KC_TRNS,   KC_NO,   KC_F7,   KC_F8,   KC_F9,     KC_NO,     KC_NO, /**/ KC_NO,    KC_NO,      KC_F1,   KC_F2,   KC_F3,   KC_NO,   KC_TRNS, \
                   KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,     KC_NO, /**/ KC_NO,    KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, \
                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS, /**/ KC_INS,   KC_TRNS,    KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                     KC_TRNS,                                         /**/                                         KC_TRNS
